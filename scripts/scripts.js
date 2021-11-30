cf388a5f8c43e138a8b91b4c805b0920e71d2bea
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2021-12-01 01:43:58 +0300

    после 1 ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,35 @@
-let anchor = document.querySelector(".profile__edit-button");
-let modal = document.querySelector(".modal");
-let closeButton = document.querySelector(".modal__close-icon");
-let saveButton = document.querySelector(".modal__button");
+let anchor = document.querySelector('.profile__edit-button');
+let modal = document.querySelector('.modal');
+let closeButton = document.querySelector('.modal__close-icon');
+let formElement = document.querySelector('.modal__form');
+let nameInput = document.querySelector('.modal__field_name');
+let jobInput = document.querySelector('.modal__field_occupation');
+let username = document.querySelector('.profile__name');
+let userjob = document.querySelector('.profile__occupation');
+
 function openModal() {
-    modal.classList.add("modal_active");
+
+    modal.classList.add('modal_active');
+    nameInput.value = username.textContent;
+    jobInput.value = userjob.textContent;
+
 }
-anchor.addEventListener("click", openModal);
+
 function closeModal() {
-    modal.classList.remove("modal_active");
+
+    modal.classList.remove('modal_active');
+
 }
-closeButton.addEventListener("click", closeModal);
-saveButton.addEventListener("click", closeModal);
-
-let formElement = document.querySelector(".modal__form");
-let nameInput = document.querySelector(".modal__name-field");
-let jobInput = document.querySelector(".modal__occupation-field");
-let username = document.querySelector(".profile__name");
-let userjob = document.querySelector(".profile__occupation");
-
 
 function formSubmitHandler(evt) {
+
     evt.preventDefault();
-    
-    username.textContent=nameInput.value;
-    userjob.textContent=jobInput.value;
+
+    username.textContent = nameInput.value;
+    userjob.textContent = jobInput.value;
+    modal.classList.remove('modal_active');
 
 }
-
-formElement.addEventListener('submit', formSubmitHandler);
-
+anchor.addEventListener('click', openModal);
+formElement.addEventListener('submit', formSubmitHandler, closeModal);
+closeButton.addEventListener('click', closeModal);
