ed89c0c08ab5630036cf4f549e7e7d0920a69f5d
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2021-12-01 22:53:52 +0300

    После 2 ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,8 @@ let anchor = document.querySelector('.profile__edit-button');
 let modal = document.querySelector('.modal');
 let closeButton = document.querySelector('.modal__close-icon');
 let formElement = document.querySelector('.modal__form');
-let nameInput = document.querySelector('.modal__field_name');
-let jobInput = document.querySelector('.modal__field_occupation');
+let nameInput = document.querySelector('.modal__field_type_name');
+let jobInput = document.querySelector('.modal__field_type_occupation');
 let username = document.querySelector('.profile__name');
 let userjob = document.querySelector('.profile__occupation');
 
@@ -23,13 +23,11 @@ function closeModal() {
 
 function formSubmitHandler(evt) {
 
-    evt.preventDefault();
-
     username.textContent = nameInput.value;
     userjob.textContent = jobInput.value;
-    modal.classList.remove('modal_active');
-
+    evt.preventDefault();
+    closeModal();
 }
 anchor.addEventListener('click', openModal);
-formElement.addEventListener('submit', formSubmitHandler, closeModal);
+formElement.addEventListener('submit', formSubmitHandler);
 closeButton.addEventListener('click', closeModal);
