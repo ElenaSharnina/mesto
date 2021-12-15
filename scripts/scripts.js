db23309288499d70ad13af4c64a9b4152a686a43
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2021-12-15 23:43:03 +0300

    перед 2-м ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
-const anchor = document.querySelector('.profile__edit-button');
-const modal = document.querySelector('.modal');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const modalEdit = document.querySelector('.modal-edit');
 const closeButtonReg = document.querySelector('.modal__close-icon_place_regform');
 const closeButtonPic = document.querySelector('.modal__close-icon_place_modalpic');
 const formElement = document.querySelector('.modal__form_place_regform');
@@ -13,137 +13,119 @@ const addButton = document.querySelector('.profile__add-button');
 const modalCard = document.querySelector('.modal-card');
 const modalImage = document.querySelector('.modal-card__image');
 const modalLabel = document.querySelector('.modal-card__label');
-
-function openModal() {
-    modal.classList.add('modal_active');
-    nameInput.value = username.textContent;
-    jobInput.value = userjob.textContent;
-}
-
-function openModalpic() {
-    modalpic.classList.add('modal_active');
-    //value для того чтобы без обновления страницы выходила пустая форма
-    picName.value = '';
-    picLink.value = '';
-}
-
-function closeModal() {
-    modal.classList.remove('modal_active');
-}
-
-function closeModalpic() {
-    modalpic.classList.remove('modal_active');
-    
-}
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    username.textContent = nameInput.value;
-    userjob.textContent = jobInput.value;
-    closeModal();
-}
-anchor.addEventListener('click', openModal);
-formElement.addEventListener('submit', formSubmitHandler);
-closeButtonReg.addEventListener('click', closeModal);
-closeButtonPic.addEventListener('click', closeModalpic);
-addButton.addEventListener('click', openModalpic);
-
-
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
-
-//выводим карточки на страницу
+const modalCardCloseBtn = document.querySelector('.modal__close-icon_place_modal-card');
+const picName = document.querySelector('.modal__field_type_card-name');
+const picLink = document.querySelector('.modal__field_type_card-link');
 const picContainer = document.querySelector('.elements');
 const picTemplate = document.querySelector('#element').content;
 
-//цикл для отбора массива  
-for (let i = 0; i < initialCards.length; i = i + 1) {
-    const picElement = picTemplate.querySelector('.element').cloneNode(true);
-    picElement.querySelector('.element__name').textContent = initialCards[i].name;
-    picElement.querySelector('.element__image').src = initialCards[i].link;
-    //LIKE  карточки
-    picElement.querySelector('.element__like').addEventListener('click', function() {
-      picElement.querySelector('.element__like').classList.toggle('element__like_active');
-    })
-    // удаление карточки
-    picElement.querySelector('.element__delete').addEventListener('click', function() {
-      picElement.classList.add('element_delete');
-    })
-    //открытие картинки в модальном окне
-    
-    picElement.querySelector('.element__image').addEventListener('click', function() {
-      modalCard.classList.add('modal_active');
-      modalLabel.textContent = initialCards[i].name;
-      modalImage.src = initialCards[i].link;
-    })
-    //закрытие картинки в модальном окне
-    const modalCardCloseBtn = document.querySelector('.modal__close-icon_place_modal-card');
-    
-    modalCardCloseBtn.addEventListener('click', function() {
-      modalCard.classList.remove('modal_active');
-   })
-    picContainer.append(picElement);
+
+nameInput.value = username.textContent;
+jobInput.value = userjob.textContent;
+
+//функция открытия модального окна
+
+function openModal (popup) {
+  popup.classList.add('modal_active');    
 }
 
-// функция добавления новой фотографии
-const picName = document.querySelector('.modal__field_type_card-name');
-const picLink = document.querySelector('.modal__field_type_card-link');
+//value для того чтобы без обновления страницы выходила пустая форма
+
+function openAddModal () {
+  openModal(modalpic);
+  picName.value = '';
+  picLink.value = '';
+}
+
+//функция закрытия модального окна
+
+function closeModal(popup) {
+  popup.classList.remove('modal_active');
+}
+
+//функция отправки формы профиля
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  username.textContent = nameInput.value;
+  userjob.textContent = jobInput.value;
+  closeModal(modalEdit);
+}
+
+//выводим карточки на страницу из массива
+
+function addCards (item) {
+  picContainer.append(item);
+}
+
+function createCard (item) {
+  const picElement = picTemplate.querySelector('.element').cloneNode(true);
+  picElement.querySelector('.element__name').textContent = item.name;
+  picElement.querySelector('.element__image').src = item.link;
+  picElement.querySelector('.element__image').alt = item.name;
+  picContainer.addEventListener('click', deleteElement); //удаляем картинку
+  picContainer.addEventListener('click', modalImageOpen); //открытие модального окна у картинки
+  picContainer.addEventListener('click', likeActive); //лайк карточки
+  return picElement;
+}
+
+function selectionCard (array) { //отбираем элементы из массива
+  array.forEach((item) => 
+  addCards (createCard (item)));
+}
+selectionCard(initialCards);
+   
+// добавление карточки из формы
 
 function formSubmitNewCard(evt) {
   evt.preventDefault();
-  const picElement = picTemplate.querySelector('.element').cloneNode(true);
-  picElement.querySelector('.element__name').textContent = picName.value;
-  picElement.querySelector('.element__image').src = picLink.value;
-  
-  picContainer.prepend(picElement);
-  //LIKE новой карточки
-
-  picElement.querySelector('.element__like').addEventListener('click', function() {
-    picElement.querySelector('.element__like').classList.toggle('element__like_active');
-  })
-  // удаление новой карточки
-  picElement.querySelector('.element__delete').addEventListener('click', function() {
-    picElement.classList.add('element_delete');
-  })
-  // открытие модального окна для новой карточки 
-  picElement.querySelector('.element__image').addEventListener('click', function() {
-    modalCard.classList.add('modal_active');
-    modalLabel.textContent = picName.value;
-    modalImage.src = picLink.value;
-  })
-  closeModalpic();
+  const picElement = {
+    name: picName.value,
+    link: picLink.value
+  }
+  addCards(createCard(picElement));
+  picContainer.addEventListener('click', deleteElement);
+  picContainer.addEventListener('click', likeActive);
+  picContainer.addEventListener('click', modalImageOpen);
+  closeModal(modalpic);
 }
 
-formElementPic.addEventListener('submit',formSubmitNewCard);
+//like карточки
+
+function likeActive (evt) {
+  const evtLike = evt.target;
+  if (evt.target.classList.contains('element__like')) {
+  evtLike.classList.toggle('element__like_active');
+  }
+}
+
+// открытие модального окна для карточки
+
+function modalImageOpen (evt) {
+  const evtImage = evt.target;
+  if (evtImage.classList.contains('element__image')) {
+    modalLabel.textContent = evtImage.alt;
+    modalImage.src = evtImage.src;
+    modalImage.alt = evtImage.alt;
+    openModal(modalCard);
+  }
+}
+  
+
+// удаление Элемента
+
+function deleteElement (evt) {
+  const evtDelete = evt.target;
+  if (evtDelete.classList.contains('element__delete')) {
+    evt.target.closest('.element').remove();
+  }
+}
 
 
-
-
-
-
-
+formElementPic.addEventListener('submit', formSubmitNewCard);
+buttonEdit.addEventListener('click', () => openModal(modalEdit));
+formElement.addEventListener('submit', formSubmitHandler);
+closeButtonReg.addEventListener('click', () => closeModal(modalEdit));
+closeButtonPic.addEventListener('click', () => closeModal(modalpic));
+addButton.addEventListener('click', () => openAddModal());
+modalCardCloseBtn.addEventListener('click', () => closeModal(modalCard));
